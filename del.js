e538c0c417ea0eb82096c82d0fdf5f5330bfa6c6
Haris Siddiqui <haris503@hotmail.com>
2025-01-01 19:20:57 -0500

    Upload/download/delete files from Supabase

--- del.js
+++ del.js
@@ -1,10 +1,56 @@
 // grep -nr --exclude-dir=node_modules "console.error"
+
+require("dotenv").config();
 const prisma = require("./db/client");
+const { createClient } = require("@supabase/supabase-js");
+const fs = require("fs/promises");
+const path = require("path");
+const { buffer } = require("stream/consumers");
+const { removeFile } = require("./utils");
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+);
 
 (async () => {
-  console.log(await prisma.file.findMany());
-  
-  // console.log(await prisma.user.findUnique({
-  //   where: { username: "a" },
-  // }));
+  try {
+    // console.log(await prisma.file.findMany());
+    const file = await prisma.file.findUnique({
+      where: { id: "09a7ebb0-3ae2-4fb6-b7e8-f6b46b00600c" },
+      // where: { id: "05ad45fd-bf6c-4767-9301-3b4e29b3a361" },
+    });
+
+    console.log(file);
+    const filePath = path.relative(process.cwd(), "./public/uploads/" + file.id);
+    
+    const buffer = await fs.readFile(filePath);
+    const f = new File(buffer, file.id, { type: file.mimetype });
+    console.log(f);
+    
+    
+    
+    // const { data, error } = await supabase.storage
+    //   .from('Files')
+    //   .upload(filePath, buffer);
+
+    const { data, error } = await supabase.storage
+      .from('Files')
+      .upload(filePath, buffer, { contentType: file.mimetype });
+      
+    if (error) {
+      console.error('Error uploading file:', error);
+    } else {
+      console.log('File uploaded successfully:', data);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+(async () => {
+  const str = "p28.png";
+  const filePath = path.relative(process.cwd(), "./public/uploads/" + str);
+  removeFile(str);
 })();
--- routes/files.js
+++ routes/files.js
@@ -1,13 +1,19 @@
 const path = require("path");
-const { cwd } = require("node:process");
 const { v4: uuidv4 } = require("uuid");
 const { filesize } = require("filesize");
 const { removeFile } = require("../utils/");
 const prisma = require("../db/client");
 const multer = require("multer");
 const { Router } = require("express");
-
+const { createClient } = require("@supabase/supabase-js");
+const fs = require("fs/promises");
 const { postValidation, getValidation, putValidation, deleteValidation } = require("../middleware/validation/files");
+require("dotenv").config();
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY,
+);
 
 const router = Router();
 const upload = multer({
@@ -21,40 +27,82 @@ const upload = multer({
   }),
 });
 
-router.post("/", postValidation, upload.single("uploaded_file"), async (req, res) => {
-    let newFile = await prisma.file.create({
+router.post("/", upload.single("uploaded_file"), postValidation, async (req, res) => {
+  try {
+    const filePath = path.relative(
+      process.cwd(),
+      "./public/uploads/" + req.file.filename
+    );
+
+    const buffer = await fs.readFile(filePath);
+
+    const { data, error } = await supabase.storage
+      .from("Files")
+      .upload(filePath, buffer, { contentType: req.file.mimetype });
+      
+    if (error) {
+      console.error("Error uploading file:", error);
+      return res.status(500).send(error);
+    }
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    removeFile(req.file.filename);
+  }
+
+  let newFile = await prisma.file.create({
+    data: {
+      id: req.file.filename,
+      name: req.file.originalname,
+      mimetype: req.file.mimetype,
+      size: req.file.size,
+      user: { connect: { id: req.user.id } },
+    },
+  });
+
+  if (req.body.parentFolderId !== undefined) {
+    newFile = await prisma.file.update({
+      where: { id: newFile.id },
       data: {
-        id: req.file.filename,
-        name: req.file.originalname,
-        mimetype: req.file.mimetype,
-        size: req.file.size,
-        user: { connect: { id: req.user.id } },
+        parentFolder: { connect: { id: Number(req.body.parentFolderId) } },
       },
     });
+  }
 
-    if (req.body.parentFolderId !== undefined) {
-      newFile = await prisma.file.update({
-        where: { id: newFile.id },
-        data: {
-          parentFolder: { connect: { id: Number(req.body.parentFolderId) } },
-        },
-      });
-    }
+  if (req.body.parentFolderId) {
+    return res.redirect(`/folders/${req.body.parentFolderId}`);
+  }
+  res.redirect("/folders");
+});
 
-    if (req.body.parentFolderId) {
-      return res.redirect(`/folders/${req.body.parentFolderId}`);
-    }
-    res.redirect("/folders");
-  },
-);
-
-router.get("/:fileId/download", getValidation,async (req, res) => {
+router.get("/:fileId/download", getValidation, async (req, res) => {
   const file = await prisma.file.findUnique({
     where: { id: req.params.fileId },
   });
 
-  const filePath = path.relative(cwd(), "./public/uploads/" + file.id);
-  res.download(filePath, file.name);
+  try {
+    const { data, error } = await supabase.storage.from("Files").download(`public/uploads/${file.id}`);
+    
+    if (error) {
+      console.error("Error downloading file:", error);
+      return res.status(500).send(error);
+    }
+
+    const filePath = path.relative(process.cwd(), "./public/uploads/" + file.name);
+    const buffer = Buffer.from( await data.arrayBuffer() );
+    await fs.writeFile(filePath, buffer);
+    res.download(filePath, file.name, function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        removeFile(file.name);
+      }
+    });
+  } catch (e) {
+    res.status(500).send(e);
+    console.error(e);
+  } 
 });
 
 router.get("/:fileId", getValidation, async (req, res) => {
@@ -85,7 +133,7 @@ router.put("/:fileId", putValidation, async (req, res) => {
 
 router.delete("/:fileId", deleteValidation, async (req, res) => {
   const { fileId } = req.params;
-  removeFile(fileId);
+  await supabase.storage.from("Files").remove(`public/uploads/${fileId}`);
   await prisma.file.delete({ where: { id: fileId } });
   res.sendStatus(200);
 });
