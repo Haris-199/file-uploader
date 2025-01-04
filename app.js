07dde4c239025df6c1863c7452023f3f06dcc1ff
Haris Siddiqui <haris503@hotmail.com>
2025-01-04 05:39:55 -0500

    Add user to res.locals

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use((req, res, next) => {
+  if (req.user) res.locals.user = req.user;
+  next();
+});
 app.use("/", router);
 app.use(serverErrorHandler);
 
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render(".", {view: "home"});
+  res.render(".", { view: "home" });
 });
 
 module.exports = router;
